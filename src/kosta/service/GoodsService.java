d7c0f66b15c68e2e828ee14c79095b7f57eca267
heejung <8253jang@daum.net>
2026-02-09 16:04:28 +0900

    시나리오3번 - GoodsService수정

--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -4,4 +4,9 @@ public class GoodsService {
    public void insert() {
 	   System.out.println("insert call..");
    }
+   
+   public void select() {
+	   System.out.println("select추가..");
+	   System.out.println("시니리오 3번 test");
+   }
 }
